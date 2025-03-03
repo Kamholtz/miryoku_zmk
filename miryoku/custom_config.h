7c533714d91b733b8fda4c6bfff55835b69d67a2
Carl Kamholtz <carl.kamholtz@gmail.com>
2025-03-03 20:26:10 +1000

    fix: missing keys

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,4 +6,4 @@
 &kp TAB,      &kp Q,               &kp W,              &kp E,              &kp R,              &kp T,              &kp DOWN,        &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,                 &kp PIPE, \
 &kp LCTRL,    U_MTL(LGUI, A),      U_MTL(LALT, S),     U_MTL(LCTRL, D),    U_MTL(LSHFT, F),    &kp G,              &kp LEFT,        &kp H,             U_MTR(LSHFT, J),   U_MTR(LCTRL, K),   U_MTR(LALT, L),    U_MTR(LGUI, SQT),      &kp DOT,  \
 &kp LSHFT,    U_LT(U_BUTTON, Z),   U_MTL(RALT, X),     &kp C,              &kp V,              &kp B,              &kp RIGHT,       &kp N,             &kp M,             &kp COMMA,         U_MTR(RALT, DOT),  U_LT(U_BUTTON, SLASH), &kp RET, \
-              &kp LCTRL, &kp LGUI, U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),   &kp RET,         U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  &kp RSHFT,         &kp RALT
+              &kp LCTRL, &kp LGUI, U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET),  U_NP,  &kp RET,    U_NP,      U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  &kp RSHFT,         &kp RALT
