26ea76c4a2e93872a1b89b8692a863cee80296be
Carl Kamholtz <carl.kamholtz@gmail.com>
2025-03-05 21:26:24 +1000

    wip: try remap sym

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -36,24 +36,9 @@ XXX               &kp LCTRL &kp LGUI  K32     K33  K34    &kp RET      K35  K36 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_RIGHT 55 56
 
 
-/*#define MIRYOKU_ALTERNATIVES_SYM \*/
-/*&kp LBRC,          &kp N4,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \*/
-/*&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \*/
-/*&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_STU,             \*/
-/*U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP*/
-/**/
-#if !defined (MIRYOKU_KEYBOARD_EYELASH_SOFLE)
+#define MIRYOKU_LAYER_SYM \
+&kp LBRC,          &kp N4,          &kp ASTRK,         &kp LPAR,          &kp RBRC,          U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_BOOT,            \
+&kp COLON,         &kp DLLR,          &kp PRCNT,         &kp CARET,         &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp TILDE,         &kp EXCL,          &kp AT,            &kp HASH,          &kp PIPE,          U_NA,              &u_to_U_SYM,       &u_to_U_MOUSE,     &kp RALT,          U_STU,             \
+U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
-#define MIRYOKU_LAYERMAPPING_SYM( \
-     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
-) \
-XXX  XXX  XXX  XXX  XXX  XXX         XXX         XXX  XXX  XXX  XXX  XXX  XXX \
-XXX  K00  &kp N4  K02  K03  K04         XXX         K05  K06  K07  K08  K09  XXX \
-XXX  K10  K11  K12  K13  K14         XXX         K15  K16  K17  K18  K19  XXX \
-XXX  K20  K21  K22  K23  K24         XXX         K25  K26  K27  K28  K29  XXX \
-XXX       XXX  K32  K33  K34  XXX    XXX    XXX  K35  K36  K37  XXX
-
-#endif
