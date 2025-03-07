de8d9fa6054fa72d28ceb4007da2dc1a7defccc0
Carl Kamholtz <carl.kamholtz@gmail.com>
2025-03-07 20:55:24 +1000

    feat: reset

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,16 +3,31 @@
 
 #define XXX &none
 
+
+#define MIRYOKU_LAYOUTMAPPING_SOFLE( \
+     K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
+) \
+&kp ESC     XXX  XXX  XXX  XXX  XXX         XXX         XXX  XXX  XXX  XXX  XXX  XXX \
+&kp N1      K00  K01  K02  K03  K04         XXX         K05  K06  K07  K08  K09  XXX \
+XXX         K10  K11  K12  K13  K14         XXX         K15  K16  K17  K18  K19  XXX \
+XXX         K20  K21  K22  K23  K24         XXX         K25  K26  K27  K28  K29  XXX \
+XXX              XXX  K32  K33  K34  XXX    XXX    XXX  K35  K36  K37  XXX
+
+#define XXX &none
+
 #define MIRYOKU_LAYOUTMAPPING_EYELASH_SOFLE( \
      K00, K01, K02, K03, K04,                K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,                K15, K16, K17, K18, K19, \
      K20, K21, K22, K23, K24,                K25, K26, K27, K28, K29, \
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
-&kp ESC   &kp N1  &kp N2    &kp N3    &kp N4  &kp N5      &kp UP            &kp N6  &kp N7  &kp N8     &kp N9   &kp N0  &kp N1 \
-&kp TAB   K00     K01       K02       K03     K04         &kp DOWN          K05     K06     K07        K08      K09     &kp N2 \
-&kp LCTRL K10     K11       K12       K13     K14         &kp LEFT          K15     K16     K17        K18      K19     &kp N3  \
-&kp LSHFT K20     K21       K22       K23     K24         &kp RIGHT         K25     K26     K27        K28      K29     &kp N4  \
+&kp ESC   &kp N1  &kp N2    &kp N3    &kp N4  &kp N5      &kp UP            &kp N6  &kp N7  &kp N8     &kp N9   &kp N0  &kp BSPC \
+&kp TAB   K00     K01       K02       K03     K04         &kp DOWN          K05     K06     K07        K08      K09     &kp PIPE \
+&kp LCTRL K10     K11       K12       K13     K14         &kp LEFT          K15     K16     K17        K18      K19     &kp SQT  \
+&kp LSHFT K20     K21       K22       K23     K24         &kp RIGHT         K25     K26     K27        K28      K29     &kp RET  \
 XXX               &kp LCTRL &kp LGUI  K32     K33  K34    &kp RET      K35  K36     K37     &kp RSHFT  &kp RALT
 
 #define MIRYOKU_KLUDGE_THUMBCOMBOS_LEFT 53 54
