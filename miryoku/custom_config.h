014d2c62ee3a640f64e3c254ee80f76acb0b9786
Carl Kamholtz <carl.kamholtz@gmail.com>
2025-03-08 22:45:53 +1000

    wip: change undo to ctrl+z and change pipe to slash on base

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -10,7 +10,7 @@
      N30, N31, K32, K33, K34,                K35, K36, K37, N38, N39 \
 ) \
 &kp ESC     &kp N1    &kp N2      &kp N3      &kp N4    &kp N5         &kp UP              &kp N6    &kp N7    &kp N8       &kp N9     &kp N0    &kp MINUS  \
-&kp TAB     K00       K01         K02         K03       K04            &kp DOWN            K05       K06       K07          K08        K09       &kp PIPE  \
+&kp TAB     K00       K01         K02         K03       K04            &kp DOWN            K05       K06       K07          K08        K09       &kp SLASH  \
 &kp LCTRL   K10       K11         K12         K13       K14            &kp LEFT            K15       K16       K17          K18        K19       &kp SQT   \
 &kp LSHFT   K20       K21         K22         K23       K24            &kp RIGHT           K25       K26       K27          K28        K29       &kp RET   \
 XXX                   &kp LCTRL   &kp LGUI    K32       K33     K34    &kp RET      K35    K36       K37       &kp RSHFT    &kp RALT
